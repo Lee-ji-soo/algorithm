67fdf3c55e5dc29922a9d82620bd86806f75e7ff
Lee-ji-soo <nbsp3@naver.com>
2022-10-01 01:16:31 +0900

    fix indent

--- stack/stackNumbers.py
+++ stack/stackNumbers.py
@@ -1,30 +1,25 @@
 # [백준] 1874번 스택 수열 (Python 파이썬)
 # https://www.acmicpc.net/problem/1874
 import sys
-def solution(n):
-  stack = []
-  answer = []
-  flag = 0
-  cur = 1
-  for i in range(n):
-    num = int(sys.stdin.readline());
-    while cur <= num:
-      stack.append(cur)
-      answer.append('+')
-      cur += 1      
 
-    print(stack, cur)
-    if stack[-1] == num:
-      stack.pop()
-      answer.append('-')
-    else:
-      print("NO")
-      flag = 1
-      break
-
-  if flag == 0:
-    for i in answer:
-      print(i)
-  
 N = int(sys.stdin.readline())
-solution(N)
+stack = []
+answer = []
+flag = 0
+cur = 1
+for i in range(N):
+  num = int(sys.stdin.readline());
+  while cur <= num:
+    stack.append(cur)
+    answer.append('+')
+    cur += 1      
+  if stack[-1] == num:
+    stack.pop()
+    answer.append('-')
+  else:
+    print("NO")
+    flag = 1
+    break
+if flag == 0:
+  for i in answer:
+    print(i)
