d370c9b32b6578ffe5d2b82e74b5e3b70de27242
Lee-ji-soo <nbsp3@naver.com>
2022-10-03 00:53:50 +0900

    천원단위 구분하기

---
+++ stack/천원단위.py
@@ -0,0 +1,10 @@
+import sys
+unit = sys.stdin.readline().strip()
+num = sys.stdin.readline().strip()
+
+nums = []
+for i in range(int(unit)-1, -1, -1):
+  nums.append(num[i])
+  if (i != 0 and i % 3 == 0):
+    nums.append(',')
+print(''.join(nums))
