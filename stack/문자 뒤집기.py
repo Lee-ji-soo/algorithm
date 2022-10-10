7706004b557aff3745b04e61478777954e73af56
Lee-ji-soo <nbsp3@naver.com>
2022-10-11 00:33:07 +0900

    문자열 뒤집기 stack

---
+++ stack/문자 뒤집기.py
@@ -0,0 +1,27 @@
+N = int(input())
+
+#나의 풀이
+for i in range(N):
+  sen = input().split(' ')
+  arr = []
+  for a in list(sen):
+    b = a[::-1] #단어를 뒤집어서 저장해줌
+    arr.append(b)
+  print((' ').join(arr))
+
+#다른이의 풀이 >>좀더 스택 주제에 어울리는 풀이방법
+for i in range(N):
+  string = input()
+  string += " "
+  stack = []
+  for j in string:
+    print('j', j)
+    if j != " ":
+      stack.append(j)
+    else:
+      while stack:
+        # print(stack)
+        print(stack.pop(), end="")
+      print(" ", end="")
+      
+#https://sooleeandtomas.tistory.com/82
