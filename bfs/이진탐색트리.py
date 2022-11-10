0a2af632dcbc92511496bd85768d0a6c1c1755e5
Lee-ji-soo <nbsp3@naver.com>
2022-11-10 23:31:51 +0900

    bst add logic

--- bfs/이진탐색트리.py
+++ bfs/이진탐색트리.py
@@ -23,7 +23,26 @@ class BinarySearchTree():
 
 
   def add(self, key, value) -> bool:
-    
+    def add_node(node, key, value) -> None:
+      if key == node.key:
+        return False
+      elif key < node.key:
+        if node.left is None:
+          node.left = Node(key, value, None, None)
+        else:
+          add_node(node.left, key, value)
+      else:
+        if node.right is None:
+          node.right = Node(key, value, None, None)
+        else:
+          add_node(node.right, key, value)
+      return True
+    if self.root is None:
+      self.root = Node(key, value, None, None)
+      return True
+    else:
+      return add_node(self.root, key ,value)
+      
   def remove(self, key) -> bool:
 
   def dump(self) -> None:
