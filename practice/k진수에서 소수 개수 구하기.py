44449e5f922f4f9cded08b542c85d26268e08004
Lee-ji-soo <nbsp3@naver.com>
2022-12-05 21:58:01 +0900

    k 진수에서 소수 개수 구하기

--- practice/k진수에서 소수 개수 구하기.py
+++ practice/k진수에서 소수 개수 구하기.py
@@ -1,6 +1,32 @@
+def isPrime(k):
+    if k == 2 or k == 3:
+        return True
+    if k % 2 == 0 or k < 2:
+        return False
+    for i in range(3, int(k**0.5) + 1, 2):
+        if k % i == 0:
+            return False
+    return True
+
+
 def convert(n, base):
-    q, r = divmod(n, base)
-    if q == 0:
-        return str(r)
-    else:
-        return convert(q, base) + str(r)
+    k = ""
+    while n > 0:
+        k += str(n % base)
+        n = n // base
+    return "".join(reversed(k))
+
+
+def solution(n, k):
+    jinsu = convert(n, k)
+    print(jinsu)
+    answer = 0
+    for i in jinsu.split("0"):
+        if i == "":
+            continue
+        if i and isPrime(int(i)):
+            answer += 1
+    return answer
+
+
+print(solution(110011, 10))
