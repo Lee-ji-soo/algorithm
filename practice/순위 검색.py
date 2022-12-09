a83764c822da6773a712c3a75d21acb272ef3c3b
Lee-ji-soo <nbsp3@naver.com>
2022-12-09 23:08:26 +0900

    complete 순위 검색

--- practice/순위 검색.py
+++ practice/순위 검색.py
@@ -1,39 +1,54 @@
-query = [
-    "java and backend and junior and pizza 100",
-    "python and frontend and senior and chicken 200",
-    "cpp and - and senior and pizza 250",
-    "- and backend and senior and - 150",
-    "- and - and - and chicken 100",
-    "- and - and - and - 150",
-]
+def solution(info, query):
+    answer = [0 for i in range(len(query))]
+    newInfo = []
+    newQuery = []
 
-info = [
-    ["java", "backend", "junior", "pizza", 150],
-    ["python", "frontend", "senior", "chicken", 150],
-    ["cpp", "backend", "senior", "pizza", 260],
-    ["java", "backend", "junior", "chicken", 80],
-    ["python", "backend", "senior", "chicken", 50],
-]
+    for i in info:
+        newInfo.append(i.split(" "))
+    for i in query:
+        replaced = i.replace("and", "")
+        split = replaced.split(" ")
+        split = [v for v in split if v]
+        newQuery.append(split)
 
-new = []
-for i in query:
-    replace = i.replace("and", "")
-    split = replace.split(" ")
-    split = [v for v in split if v]
-    new.append(split)
-
-answer = 0
-for i in range(len(info)):
-    for j in range(len(query)):
-        flag = 0
-        for k in range(4):
-            if info[i][k] != query[j][k]:
-                if query[j][k] != "-":
-                    flag = 1
+    for i in range(len(newQuery)):
+        for j in range(len(newInfo)):
+            p = True
+            if int(newInfo[j][4]) < int(newQuery[i][4]):
+                p = False
+                continue
+            for k in range(4):
+                if newQuery[i][k] == "-":
+                    p = True
+                    continue
+                if newInfo[j][k] != newQuery[i][k]:
+                    p = False
                     break
-        if flag == 0:
-            if int(info[i][4]) >= int(query[j][4]):
-                answer[j] += 1
+                else:
+                    p = True
+            if p == True:
+                answer[i] += 1
 
-print(new)
-# [['java', 'backend', 'junior', 'pizza', '100'], ['python', 'frontend', 'senior', 'chicken', '200'], ['cpp', '-', 'senior', 'pizza', '250'], ['-', 'backend', 'senior', '-', '150'], ['-', '-', '-', 'chicken', '100'], ['-', '-', '-', '-', '150']]
+    return answer
+
+
+print(
+    solution(
+        [
+            "java backend junior pizza 150",
+            "python frontend senior chicken 210",
+            "python frontend senior chicken 150",
+            "cpp backend senior pizza 260",
+            "java backend junior chicken 80",
+            "python backend senior chicken 50",
+        ],
+        [
+            "java and backend and junior and pizza 100",
+            "python and frontend and senior and chicken 200",
+            "cpp and - and senior and pizza 250",
+            "- and backend and senior and - 150",
+            "- and - and - and chicken 100",
+            "- and - and - and - 150",
+        ],
+    )
+)
