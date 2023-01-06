5d0a7ff30a63d2cc7f945d1ade2e4216e3407e0e
Lee-ji-soo <nbsp3@naver.com>
2023-01-06 23:02:12 +0900

    문장 A 추가하기

---
+++ 훈련 설계/16-문장A추가하기.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    int index;
+    cin >> index;
+    
+    int len = 0;
+    for (int i = 0; i < 6; i++){
+        if(str[i] == '\0'){
+            len = i;
+            break;
+        }
+    }
+    char arr[len + 1] = {""};
+    for (int i = 0; i < index; i++){
+        arr[i] = str[i];
+    }
+    
+    arr[index] = 'A';
+    
+    for (int i = index + 1 ; i < len + 1; i++){
+        arr[i] = str[i - 1];
+    }
+    
+    for (int i = 0; i< len + 1; i++){
+        cout << arr[i];
+    }
+    return 0;
+}
