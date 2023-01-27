9b3686a6a23e0445dce9c7e447dbdb7cfcd5e8e0
Lee-ji-soo <nbsp3@naver.com>
2023-01-27 23:09:39 +0900

    각 글자수 세기

---
+++ 훈련 설계/18.5-각 글자수 세기.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+int main() {
+    char c[11] ={""};
+    
+    cin >> c;
+
+    int len = strlen(c);
+
+    int dat[200] = {0};
+    for(int x=0; x<len; x++){
+        dat[c[x]]++;
+    }
+    
+    for(int x=65; x<200;x++){
+        if(dat[x] > 0){
+            cout << (char)x <<':'<< dat[x] << endl;;
+        }       
+    }
+}
