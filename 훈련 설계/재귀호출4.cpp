3be86fe199ed984b4c204c646f41bc7a13dd65d8
Lee-ji-soo <nbsp3@naver.com>
2023-02-10 00:00:19 +0900

    재귀호출4

---
+++ 훈련 설계/재귀호출4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void abc(int x)
+{
+  if (x == 2)
+  {
+    return;
+  }
+  cout << x;
+  abc(x - 1);
+  cout << x;
+}
+int main()
+{
+  abc(6);
+  return 0;
+}
+
+// 65433456
