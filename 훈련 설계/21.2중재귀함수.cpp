825e1b6c749fc0575be14ccf40a6d6dda182be79
Lee-ji-soo <nbsp3@naver.com>
2023-02-21 01:37:29 +0900

    ㅈㅐ귀함수

--- 훈련 설계/21.2중재귀함수.cpp
+++ 훈련 설계/21.2중재귀함수.cpp
@@ -33,3 +33,78 @@ int main()
   return 0;
 }
 // #######
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void abc(int lev)
+{
+  if (lev == 2)
+  {
+    return;
+  }
+  abc(lev + 1);
+  cout << '#';
+  abc(lev + 1);
+  cout << '%';
+  int d = 1;
+}
+int main()
+{
+  abc(0);
+  return 0;
+}
+// #%##%%
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void abc(int lev)
+{
+  if (lev == 2)
+  {
+    return;
+  }
+  cout << 'I' << lev << ',';
+  abc(lev + 1);
+  cout << 'T' << lev << ',';
+  abc(lev + 1);
+  cout << 'X' << lev << ',';
+  int d = 1;
+}
+int main()
+{
+  abc(0);
+  return 0;
+}
+// I0,I1,T1,X1,T0,I1,X1,T1,X0
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int cnt;
+void abc(int lev)
+{
+  if (lev == 2)
+  {
+    return;
+  }
+
+  cout << cnt;
+  for (int i = 0; i < 2; i++)
+  {
+    cnt++;
+    abc(lev + 1);
+    cnt--;
+  }
+}
+
+int main()
+{
+  abc(0);
+}
+
+# 011
