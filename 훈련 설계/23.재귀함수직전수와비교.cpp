1b33c37377bcb5a9980b88b4097ce19daa3fa5a2
Lee-ji-soo <nbsp3@naver.com>
2023-03-29 00:33:56 +0900

    23.재귀함수직전수와비교

---
+++ 훈련 설계/23.재귀함수직전수와비교.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int path[3] = {0};
+void abc(int lev)
+{
+  // if(lev >= 2 && path[lev - 1] == path[lev -2]) return;
+  if (lev == 3)
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      cout << path[i];
+    }
+    cout << endl;
+    return;
+  }
+
+  for (int i = 1; i <= 6; i++)
+  {
+    if (lev > 0 && path[lev - 1] == i)
+      continue;
+    path[lev] = i;
+    abc(lev + 1);
+  }
+}
+int main()
+{
+  abc(0);
+  return 0;
+}
