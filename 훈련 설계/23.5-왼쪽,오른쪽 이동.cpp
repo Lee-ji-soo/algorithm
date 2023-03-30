3b745b4fb7b570c07c1900249d348ec0ed57e3b5
Lee-ji-soo <nbsp3@naver.com>
2023-03-31 00:12:41 +0900

    23.5-왼쪽,오른쪽 이동

---
+++ 훈련 설계/23.5-왼쪽,오른쪽 이동.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int arr[6] = {3, 5, 1, 9, 7};
+char p[5] = {""};
+
+int main()
+{
+  for (int i = 0; i < 4; i++)
+  {
+    cin >> p[i];
+  }
+
+  for (int i = 0; i < 4; i++)
+  {
+
+    if (p[i] == 'L')
+    {
+      arr[5] = arr[0];
+      for (int i = 0; i < 5; i++)
+      {
+        arr[i - 1] = arr[i];
+      }
+      arr[4] = arr[5];
+    }
+
+    if (p[i] == 'R')
+    {
+      for (int i = 4; i >= 0; i--)
+      {
+        arr[i + 1] = arr[i];
+      }
+      arr[0] = arr[5];
+    }
+  }
+
+  for (int i = 0; i < 5; i++)
+  {
+    cout << arr[i] << ' ';
+  }
+  return 0;
+}
