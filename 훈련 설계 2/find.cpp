84b0737340f790c17bc38fff2698f2372546bd7a
Lee-ji-soo <nbsp3@naver.com>
2023-04-14 02:13:30 +0900

    find

--- 훈련 설계 2/find.cpp
+++ 훈련 설계 2/find.cpp
@@ -21,3 +21,38 @@ int main()
     }
     return 0;
 }
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string arr[4] = {
+        "ABBT",
+        "BTBT",
+        "BBBT",
+        "KFC"};
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (arr[i].find("BB") == -1)
+        {
+            cout << 'X';
+        }
+        else
+        {
+            cout << 'O';
+        }
+    }
+
+    return 0;
+}
