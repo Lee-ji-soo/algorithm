2caed8f757006aa9963570983c7ce16696b5a311
Lee-ji-soo <nbsp3@naver.com>
2023-04-14 23:45:37 +0900

    숫자 찾아 더하기. find

--- 훈련 설계 2/숫자찾아더하기.cpp
+++ 훈련 설계 2/숫자찾아더하기.cpp
@@ -50,3 +50,30 @@ int main()
     cout << sum;
     return 0;
 }
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str = "B[45]AB[9944]";
+    int a = str.find('[');
+    int b = str.find(']', a + 1);
+    int c = str.find('[', b + 1);
+    int d = str.find(']', c + 1);
+
+    int num = 0;
+    string num1 = str.substr(a + 1, b - a - 1);
+    string num2 = str.substr(c + 1, d - c - 1);
+    // cout << num1 << num2;
+    cout << stoi(num1) + stoi(num2);
+}
