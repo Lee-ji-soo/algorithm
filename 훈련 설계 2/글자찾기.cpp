771a451dc72ebe9b7c1a9076b7c302e5027eb817
Lee-ji-soo <nbsp3@naver.com>
2023-04-15 00:10:36 +0900

    GOLD찾기.cpp

--- 훈련 설계 2/글자찾기.cpp
+++ 훈련 설계 2/글자찾기.cpp
@@ -52,3 +52,41 @@ int main()
         cnt++;
     }
 }
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string arr[4] = {
+        "GOLDABGOLD",
+        "GOLDTTT",
+        "AGOLDGOLD",
+        "GOLDTTTT"};
+
+    int cnt = 0;
+    int index = -1;
+    for (int y = 0; y < 4; y++)
+    {
+        while (1)
+        {
+            index = arr[y].find("GOLD", index + 1);
+            int q = 0;
+            if (index == -1)
+            {
+                break;
+            }
+            cnt++;
+        }
+    }
+    cout << cnt;
+}
