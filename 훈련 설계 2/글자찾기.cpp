0564575727a1146845f8c54e69c1678adb378c4c
Lee-ji-soo <nbsp3@naver.com>
2023-04-14 23:59:36 +0900

    글자찾기 with one a.cpp

--- 훈련 설계 2/글자찾기.cpp
+++ 훈련 설계 2/글자찾기.cpp
@@ -26,3 +26,29 @@ int main()
     }
     cout << cnt;
 }
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string a = "AS[ABC]EKFLE[ABC]JT[ABC]";
+    int b = -1;
+    int cnt = 0;
+    while (1)
+    {
+        b = a.find("[ABC]", b + 1);
+        if (b == -1)
+            break;
+        cnt++;
+    }
+}
