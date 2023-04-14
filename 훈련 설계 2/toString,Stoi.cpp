38d690759fcae0b4e7ce1a2e20ab85f6afdc4344
Lee-ji-soo <nbsp3@naver.com>
2023-04-14 23:17:34 +0900

    to_string,stoi

---
+++ 훈련 설계 2/toString,Stoi.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str = "2432";
+    int i = stoi(str);
+    cout << i;
+
+    int num = 234;
+    string a = to_string(num);
+    cout << a;
+    return 0;
+}
