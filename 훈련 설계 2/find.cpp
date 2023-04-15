ab5d2f2db619aa584dc9e78b856112b43dfe1224
Lee-ji-soo <nbsp3@naver.com>
2023-04-16 00:23:30 +0900

    find use func.cpp

--- 훈련 설계 2/find.cpp
+++ 훈련 설계 2/find.cpp
@@ -116,3 +116,50 @@ int main()
     cout << str.substr(a, b - a + 1);
     return 0;
 }
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+string a = "AAABCDATRBBKFCKFCKFC";
+
+int find(string f)
+{
+    int cnt = 0;
+    int index = -1;
+    while (1)
+    {
+        index = a.find(f, index + 1);
+        int w = index;
+        if (index == -1)
+        {
+            break;
+        };
+        cnt++;
+    }
+    return cnt;
+}
+
+int main()
+{
+
+    string input[3] = {""};
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> input[i];
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << find(input[i]) << endl;
+    }
+    return 0;
+}
