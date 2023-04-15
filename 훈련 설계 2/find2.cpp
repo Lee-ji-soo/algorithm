b3162512cf344ee00a4ad5a77c1198e8822605e3
Lee-ji-soo <nbsp3@naver.com>
2023-04-16 01:41:33 +0900

    find

--- 훈련 설계 2/find2.cpp
+++ 훈련 설계 2/find2.cpp
@@ -98,3 +98,154 @@ int main()
         cout << input.substr(aindex + 1, bindex - aindex - 1) << endl;
     }
 }
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string input = "ABC[BTS]KK[KFC]GGR[TT]RR[STKR]";
+
+    int a = -1;
+    int b = 0;
+
+    while (1)
+    {
+        a = input.find("[", a + 1);
+        b = input.find("]", a + 1);
+
+        if (a == -1)
+        {
+            break;
+        }
+
+        string sub = input.substr(a + 1, b - a - 1);
+        cout << sub << endl;
+
+        a = b + 1;
+    }
+}
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str = "1942|1995|87";
+    // 마지막 파싱을 위해 |를 추가
+    str += "|";
+
+    int a = -1;
+    int i = 0;
+    while (1)
+    {
+        a = str.find('|', a + 1);
+        if (a == -1)
+        {
+            break;
+        }
+        cout << str.substr(i, a - i) << endl;
+        i = a + 1;
+    }
+}
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str = "123-32-43543-12";
+    str += "-";
+    // stoi
+
+    int sum = 0;
+
+    int index = -1;
+    int i = 0;
+
+    while (1)
+    {
+
+        index = str.find("-", index + 1);
+
+        if (index == -1)
+        {
+            break;
+        }
+
+        string temp = str.substr(i, index - i);
+        sum += stoi(temp);
+
+        i = index + 1;
+    }
+
+    cout << sum;
+}
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string a = "ID:48,BTS:56,HHTT:99";
+    a += ",";
+
+    int opener = -1;
+    int closer = -1;
+    int sum = 0;
+
+    while (1)
+    {
+
+        opener = a.find(":", opener + 1);
+        closer = a.find(",", opener + 1);
+
+        if (opener == -1)
+        {
+            break;
+        }
+
+        string temp = a.substr(opener + 1, closer - opener - 1);
+        int q = 0;
+        sum += stoi(temp);
+
+        opener = closer + 1;
+    }
+    cout << sum;
+}
