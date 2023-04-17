827f8da48b2c20f445f0e849f9f9dd4968f0b922
Lee-ji-soo <nbsp3@naver.com>
2023-04-18 02:04:45 +0900

    좋은이름작명소.valid

---
+++ 훈련 설계 2/25.5.7.좋은이름작명소.valid.cpp
@@ -0,0 +1,63 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int isGood(string str)
+{
+    int dat[150] = {0};
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] < 'a' || str[i] > 'z')
+        {
+            return 0;
+        }
+
+        dat[str[i]]++;
+        if (dat[str[i]] == 3)
+        {
+            return 0;
+        }
+    }
+
+    int sum = dat['a'] + dat['e'] + dat['i'] + dat['o'] + dat['u'];
+    if (sum != 3)
+    {
+        return 0;
+    }
+    int q = 0;
+    return 1;
+}
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+
+    string arr[n] = {""};
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int flag = isGood(arr[i]);
+        if (flag == 1)
+        {
+            cout << "good";
+        }
+        else
+        {
+            cout << "no";
+        }
+        cout << endl;
+    }
+}
