06955fcdb72f8e47de119735244698504fa84906
Lee-ji-soo <nbsp3@naver.com>
2023-04-18 22:19:34 +0900

    pointer

--- 훈련 설계 2/linkedList/포인터.cpp
+++ 훈련 설계 2/linkedList/포인터.cpp
@@ -79,5 +79,66 @@ int main()
     AA *p = &dic;
 
     cout << p->a;
+
+    // 화살표는 int pointer에 쓸 수 없다.
+    // 화살표는 구조체 변수를 가르키는 포인터에서만 사용할 수 있다.
     return 0;
+}
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    struct KFC
+    {
+        int a;
+        char b;
+        char c;
+    };
+
+    KFC z = {3, 'Q', 'H'};
+
+    KFC *t = &z;
+    KFC *Q = &z;
+
+    Q->a = 4;
+    cout << (t->a) + (Q->a);
+}
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    struct KFC
+    {
+        int a;
+        char *p;
+    };
+
+    char ch = 'Q';
+
+    KFC kfc = {5, &ch};
+    cout << *kfc.p;
 }
