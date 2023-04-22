3e7457d0255fc725b72029d29ca5201e5cfb8b99
Lee-ji-soo <nbsp3@naver.com>
2023-04-22 23:51:22 +0900

    queue

---
+++ 훈련 설계 2/큐/큐.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+queue<int> q;
+int main()
+{
+    q.push(1);
+    q.push(2);
+    q.push(5);
+    q.push(7);
+
+    cout << q.front() << endl;
+    q.pop();
+    cout << q.front() << endl;
+    // 1,2,5,7 // q.front() : 1
+
+    while (!q.empty())
+    {
+        cout << q.front() << endl;
+        q.pop();
+    }
+
+    cout << q.front();
+}
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+vector<vector<int>> vect(7);
+queue<int> q3;
+
+int main()
+{
+    vect[5] = {3, 1};
+    vect[3] = {2};
+    vect[1] = {4};
+    vect[4] = {0, 6};
+
+    q3.push(5);
+
+    while (!q3.empty())
+    {
+        int temp = q3.front();
+        cout << temp << " ";
+        q3.pop();
+
+        for (int i = 0; i < vect[temp].size(); i++)
+        {
+            q3.push(vect[temp][i]);
+        }
+    }
+}
