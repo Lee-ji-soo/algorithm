ba51a20ff98cb9397b97048b68af4d77a74b428a
Lee-ji-soo <nbsp3@naver.com>
2023-04-22 22:00:39 +0900

    움직이는 몬스터2

--- 훈련 설계 2/practice/29.5.8.움직이는 몬스터.cpp
+++ 훈련 설계 2/practice/29.5.8.움직이는 몬스터.cpp
@@ -80,3 +80,78 @@ int main()
     }
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+char vect[4][4] = {""};
+
+struct Node
+{
+    char ch;
+    int x;
+    int y;
+};
+
+Node player[20];
+
+int direct[4][2] = {
+    0, 1,
+    1, 0,
+    0, -1,
+    -1, 0};
+
+void move(int i)
+{
+    int dir = i % 4;
+
+    for (int t = 0; t < 3; t++)
+    {
+        int dy = 0;
+        int dx = 0;
+        dy = player[t].y + direct[dir][0];
+        dx = player[t].x + direct[dir][1];
+
+        if (vect[dy][dx] == '_')
+        {
+            vect[player[t].y][player[t].x] = '_';
+            player[t].y = dy;
+            player[t].x = dx;
+            vect[dy][dx] = player[t].ch;
+        }
+    }
+}
+
+int main()
+{
+    int n = 0;
+    for (int y = 0; y < 4; y++)
+    {
+        for (int x = 0; x < 3; x++)
+        {
+            cin >> vect[y][x];
+            if (vect[y][x] == 'A' || vect[y][x] == 'C' || vect[y][x] == 'D')
+            {
+                player[n].ch = vect[y][x];
+                player[n].x = x;
+                player[n].y = y;
+                n++;
+            }
+        }
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        move(i);
+    }
+
+    for (int y = 0; y < 4; y++)
+    {
+        for (int x = 0; x < 3; x++)
+        {
+            cout << vect[y][x];
+        }
+        cout << endl;
+    }
+    return 0;
+}
