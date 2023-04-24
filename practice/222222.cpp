554e2267e3bf8273739b1a3311407e22ec7a0e15
Lee-ji-soo <nbsp3@naver.com>
2023-04-25 00:43:41 +0900

    2222222222

---
+++ practice/222222.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int i = 22;
+    int k = 9;
+
+    int answer = 1;
+    int r = n % k;
+
+    while (r != 0)
+    {
+        r = (r * 10 + n) % k;
+        answer++;
+    }
+    cout << cnt;
+    return 0;
+}
