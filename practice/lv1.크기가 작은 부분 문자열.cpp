b9ef0a3c52e04a731f73aa437801497052355870
Lee-ji-soo <nbsp3@naver.com>
2023-05-16 00:48:36 +0900

    lv1. 크기가 작은부분 문자열

---
+++ practice/lv1.크기가 작은 부분 문자열.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int answer = 0;
+
+int solution(string t, string p)
+{
+    int plen = p.length();
+    int tlen = t.length();
+    long long pNum = stoll(p);
+
+    for (int i = 0; i <= tlen - plen; i++)
+    {
+        string sub = t.substr(i, plen);
+        long long subInt = stoll(sub);
+        if (subInt <= pNum)
+        {
+            answer++;
+        }
+    }
+    return answer;
+};
+
+int main()
+{
+    solution("10203", "15");
+    cout << answer;
+    return answer;
+}
