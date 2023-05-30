5c1976e4a9125bfcb853344a77d77f6a3bc20ef4
Lee-ji-soo <nbsp3@naver.com>
2023-05-31 00:00:48 +0900

    lv1.과일장수

---
+++ practice/lv1.과일장수.cpp
@@ -0,0 +1,71 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int compare(int a, int b)
+{
+    return a > b;
+}
+int solution(int k, int m, vector<int> score)
+{
+    sort(score.begin(), score.end(), compare);
+
+    int sum = 0;
+    for (int i = m - 1; i < score.size(); i++)
+    {
+        int min = score[i];
+        sum += (min * m);
+        i += (m - 1);
+    }
+    cout << sum;
+    return sum;
+}
+
+int main()
+{
+    solution(4, 3, {4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2});
+    return 0;
+}
+
+// 시간 초과 답변
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int compare(int a, int b)
+{
+    return a > b;
+}
+int solution(int k, int m, vector<int> score)
+{
+    sort(score.begin(), score.end(), compare);
+
+    int sum = 0;
+
+    while (true)
+    {
+        if (score.size() < m)
+        {
+            break;
+        }
+        int min = score[m - 1];
+        for (int i = 0; i < m; i++)
+        {
+            score.erase(score.begin());
+        }
+        sum += (min * m);
+    }
+    cout << sum;
+    return sum;
+}
+
+int main()
+{
+    solution(4, 3, {4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2});
+    return 0;
+}
