e8e24c6d59348280a60f961a930c8a019f88eed2
Lee-ji-soo <nbsp3@naver.com>
2023-06-07 23:21:11 +0900

    cycle을 찾아라

---
+++ 심화1/33.1.cycle을 찾아라.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int myBoss[200] = {0};
+
+int findBoss(int n)
+{
+    if (myBoss[n] == 0)
+    {
+        return n;
+    }
+
+    int boss = findBoss(myBoss[n]);
+    return boss;
+}
+
+void setBoss(int a, int b)
+{
+    int af = findBoss(a);
+    int bf = findBoss(b);
+
+    if (af == bf)
+        return;
+
+    myBoss[bf] = af;
+}
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        char z, y;
+        cin >> z >> y;
+        int a = findBoss(z);
+        int b = findBoss(y);
+
+        if (a == b)
+        {
+            cout << "발견";
+            return 0;
+        }
+        setBoss(a, b);
+    }
+    cout << "미발견";
+    return 0;
+}
