70b6993ad49d526183d3a04c682b2a0405aee8fd
Lee-ji-soo <nbsp3@naver.com>
2023-08-31 23:12:54 +0900

    foldering

--- 심화1/priorityQueue/compare5.cpp
+++ 심화1/priorityQueue/compare5.cpp
@@ -11,19 +11,23 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <cstring>
 using namespace std;
 
-struct Node {
+struct Node
+{
     string name;
     int len;
 };
 
 priority_queue<Node, vector<Node>> q;
 
-//1.작은 길이
-//2.사전순 앞에있는 이름
+// 1.작은 길이
+// 2.사전순 앞에있는 이름
 
-bool operator<(Node v, Node t){
-    if(t.len < v.len) return 1;
-    if(t.len > v.len) return 0;
+bool operator<(Node v, Node t)
+{
+    if (t.len < v.len)
+        return 1;
+    if (t.len > v.len)
+        return 0;
     return t.name < v.name;
 }
 
@@ -36,25 +40,30 @@ int main()
     q.push({"MOMS", 2});
     q.push({"MC", 4});
 
-    // 
-    while(q.size() > 1){
+    //
+    while (q.size() > 1)
+    {
         Node o = q.top();
         q.pop();
         Node t = q.top();
         q.pop();
-        int z= 1;
+        int z = 1;
+        string newName = o.name;
+        if (newName > t.name)
+            newName = t.name;
         q.push({o.name, o.len + t.len});
         int x = 1;
     }
-    
-    // BBQ1, JASON1, MOMS2, TOM2, KFC3, MC4, 
+
+    // BBQ1, JASON1, MOMS2, TOM2, KFC3, MC4,
     // BBQ2, MOMS2, TOM2, KFC3, MC4
     // TOM2, KFC3, BBQ4, MC4
     // BBQ4, MC4, TOM5
     // TOM5, BBQ8
     // TOM13
-    
-    while(!q.empty()){
+
+    while (!q.empty())
+    {
         cout << q.top().name << q.top().len << endl;
         q.pop();
     }
