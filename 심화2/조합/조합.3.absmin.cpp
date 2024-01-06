c094dd52520560f16bb3a744d1d65231e8d76702
Lee-ji-soo <nbsp3@naver.com>
2024-01-06 11:43:49 +0900

    조합.3.run함수 안에서 이중포문

--- 심화2/조합/조합.3.absmin.cpp
+++ 심화2/조합/조합.3.absmin.cpp
@@ -17,9 +17,9 @@ int map[3][4] = {
     {-2, 6, 1, 5},
     {0, 3, 15, 7},
 };
-int spreadMap[12] = {0};
+int used[3][4] = {0};
+
 int path[3] = {0};
-int used[12] = {0};
 int miniPath[3] = {0};
 int mini = 2e3;
 int target = 100;
@@ -41,28 +41,22 @@ void run(int lev)
         return;
     }
 
-    for (int i = 0; i < 12; i++)
+    for (int i = 0; i < 3; i++)
     {
-        if (used[i] == 1)
-            continue;
-        used[i] = 1;
-        path[lev] = spreadMap[i];
-        run(lev + 1);
-        used[i] = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            if (used[i][j] == 1)
+                continue;
+            used[i][j] = 1;
+            path[lev] = map[i][j];
+            run(lev + 1);
+            used[i][j] = 0;
+        }
     }
 }
 
 int main()
 {
-    int i = 0;
-    for (int y = 0; y < 3; y++)
-    {
-        for (int x = 0; x < 4; x++)
-        {
-            spreadMap[i] = map[y][x];
-            i++;
-        }
-    }
     run(0);
     cout << mini << endl;
     for (int i = 0; i < 3; i++)
