7336f068ad9cb735850cdfdec36fe2eef17f3dbc
Lee-ji-soo <nbsp3@naver.com>
2024-02-28 23:57:24 +0900

    programmers.queue.프로세스

---
+++ queue/programmers.프로세스.queue.js
@@ -0,0 +1,19 @@
+function solution(priorities, location) {
+  var answer = 0;
+  let entries = Object.entries(priorities);
+
+  while (entries.length) {
+    let top = entries[0];
+    entries = entries.slice(1, entries.length);
+    
+    if (entries.some((entry) => entry[1] > top[1])) {
+      entries = [...entries, top];
+    } else {
+      answer++;
+      if (top[0] == location) {
+        break;
+      }
+    }
+  }
+  return answer;
+}
